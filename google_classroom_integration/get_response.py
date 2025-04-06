7f90fb5bf25b91b336188b76a601ff88f52b8fed
Goutami Sooda <goutamisooda.cs22@rvce.edu.in>
2025-04-06 20:05:12 +0530

    Reorganize ocr and gitignore files

--- google_classroom_integration/get_response.py
+++ google_classroom_integration/get_response.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = "https://classroom.googleapis.com/v1/courses/764169243133/courseWork/698788015501/studentSubmissions"
-headers = {"Authorization": "Bearer ya29.a0AeXRPp4HrYvO2bmzTX3dO6Vk09BSaInwyazC3GTQGyI8yTsN_oAeYAVFTo4I7eMRL6CvH_zKX6aDbcN_TebvhiNSDb4k0WO98IHV7VN63aDqOzEW1fZ4QD07qkXv543kdnjTt98kj61Do_gQibCjkxVa_CiozAWGCVW2nb2VaCgYKAeQSARMSFQHGX2MinvTkPeFOwlP7lIdA017PCw0175"}
+url = "https://classroom.googleapis.com/v1/courses/COURSE_ID/courseWork/ASSIGNMENT_ID/studentSubmissions"
+headers = {"Authorization": "Bearer ACCESS_TOKEN"}
 
 response = requests.get(url, headers=headers)
 data = response.json()
